c3ad630f33bb0272a4995c430eb0081bf1cbf410
Sebin Suresh <sebinsuresh@gmail.com>
2026-02-26 13:23:40 -0600

    feat: add filter to secure specific endpoints

--- spring_bff/src/main/java/com/ssuresh/spring_bff/configuration/SecurityFilter.java
+++ spring_bff/src/main/java/com/ssuresh/spring_bff/configuration/SecurityFilter.java
@@ -1,7 +1,26 @@
 package com.ssuresh.spring_bff.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityFilter {    
+@EnableWebSecurity
+public class SecurityFilter {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                    .defaultSuccessUrl("/secure", false)
+                )
+                .build();
+    }
+
 }
