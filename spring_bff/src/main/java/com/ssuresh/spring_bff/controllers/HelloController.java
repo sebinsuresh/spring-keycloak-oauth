1053ade82c19a2aab81b36844c2768ab52681b96
Sebin Suresh <sebinsuresh@gmail.com>
2026-02-27 11:58:51 -0600

    feat: show user and auth details in secure endpoint

--- spring_bff/src/main/java/com/ssuresh/spring_bff/controllers/HelloController.java
+++ spring_bff/src/main/java/com/ssuresh/spring_bff/controllers/HelloController.java
@@ -1,10 +1,19 @@
 package com.ssuresh.spring_bff.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private final OAuth2AuthorizedClientService clientService = null;
 
     @GetMapping("/")
     public String hello() {
@@ -12,7 +21,21 @@ public class HelloController {
     }
 
     @GetMapping("/secure")
-    public String helloSecure() {
-        return "Hello secure world";
+    public String helloSecure(
+            @AuthenticationPrincipal OidcUser user,
+            Authentication authentication) {
+
+        String s = "Hello %s<br />".formatted(user.getFullName());
+        // s += "Your claims are %s<br />".formatted(user.getClaims().keySet());
+        // s += "Your authorities are %s".formatted(user.getAuthorities());
+
+        OAuth2AuthorizedClient client = clientService.loadAuthorizedClient(
+                "keycloak",
+                authentication.getName());
+        var token = client.getAccessToken().getTokenValue();
+
+        s += "The access token is: '%s'".formatted(token);
+
+        return s;
     }
 }
