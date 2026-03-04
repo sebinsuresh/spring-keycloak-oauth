1d92fa15f0368d2b96ded4e1b2d108fd8eb09896
Sebin Suresh <sebinsuresh@gmail.com>
2026-03-04 15:15:17 -0600

    feat: add admin service

---
+++ angular_app/src/app/core/services/admin.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { catchError, Observable, of, shareReplay } from "rxjs";
+
+export interface UserInformation {
+    userName: string,
+    fullName: string,
+}
+
+@Injectable({
+    providedIn: "root",
+})
+export class AdminService {
+    private readonly _http = inject(HttpClient);
+    private _userInfo$: Observable<Array<UserInformation> | null> | null = null;
+
+    listUsers(): Observable<Array<UserInformation> | null> {
+        if (!this._userInfo$) {
+            this._userInfo$ = this._http
+                .get<Array<UserInformation>>("/api/admin/userdata")
+                .pipe(
+                    catchError(err => {
+                        console.error(
+                            "/admin/userdata endpoint failed with error",
+                            err
+                        );
+                        return of(null);
+                    }),
+                    shareReplay(1),
+                );
+        }
+        return this._userInfo$;
+    }
+}
