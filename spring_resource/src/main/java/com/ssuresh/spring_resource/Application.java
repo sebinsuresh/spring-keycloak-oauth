1c82a9ccb3a2adf11b6656239a2840b3744a3628
Sebin Suresh <sebinsuresh@gmail.com>
2026-03-07 19:54:47 -0600

    fix: rename root class

--- spring_resource/src/main/java/com/ssuresh/spring_resource/Application.java
+++ spring_resource/src/main/java/com/ssuresh/spring_resource/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringResourceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringResourceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
