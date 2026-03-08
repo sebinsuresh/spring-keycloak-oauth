1735e4d63663de0530c5369e6096f418f2f3a31f
Sebin Suresh <sebinsuresh@gmail.com>
2026-03-07 19:45:27 -0600

    feat: add resource server to move resource handling and access config to

---
+++ spring_resource/src/main/java/com/ssuresh/spring_resource/SpringResourceApplication.java
@@ -0,0 +1,13 @@
+package com.ssuresh.spring_resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringResourceApplication.class, args);
+	}
+
+}
---
+++ spring_resource/src/test/java/com/ssuresh/spring_resource/SpringResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssuresh.spring_resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
